9640767f153701785905762ff5fafa8affa4209e
anjali313477 <anjalitadiyal5@outlook.com>
2025-07-16 21:46:23 +0530

    chnaged the folder

--- src/pages/Home/Section_c.jsx
+++ src/pages/Home/Section_c.jsx
@@ -3,14 +3,7 @@ import React from 'react'
 const Section_c = () => {
   return (
     <>
-    <div className='bg-[#0B0B0B] pt-[46px] pb-[106px]'>
-    <div className="main max-w-[1250px] m-auto">
-      <div className="main_wrapper text-center relative">
-        <div className="heading_text xl:text-[205px] lg:text-[155px] md:text-[95px] text-[65px] ">WEB BLAZE</div>
-        <div className="para_text xl:text-[35px] lg:text-[30px] md:text-[30px] text-[16px] text-[#EBE8C9] font-normal absolute -bottom-[5px] md:-bottom-[30px] lg:-bottom-[5px] xl:-bottom-[2px]">Combine the power of AI and Automation to streamline workflows and drive measurable results</div>
-      </div>
-    </div>
-    </div>
+
     </>
   )
 }
